9968d77be866d295c345d3b736284290d74ac3da
Fei Heng <hf2004418@163.com>
2021-04-29 14:45:57 -0400

    add simulated data

---
+++ R/simdata.R
@@ -0,0 +1,35 @@
+#' Simulated competing risks data with 2 causes subject to missingness
+#'
+#' Simulated data for the package cmprskPH
+#'
+#' @docType data
+#'
+#' @usage data(sim2cs)
+#'
+#' @format An object of class data.frame
+#'
+#' @keywords datasets
+#'
+#' @references Heng et.al. (2021+)
+#'
+#' @examples
+#' data(sim2cs)
+"sim2cs"
+
+#' Simulated competing risks data with 3 causes subject to missingness
+#'
+#' Simulated data for the package cmprskPH
+#'
+#' @docType data
+#'
+#' @usage data(sim3cs)
+#'
+#' @format An object of class data.frame
+#'
+#' @keywords datasets
+#'
+#' @references Heng et.al. (2021+)
+#'
+#' @examples
+#' data(sim3cs)
+"sim3cs"
